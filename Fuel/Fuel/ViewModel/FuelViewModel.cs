6dc28f217b73d88327e8293f8a690826be3fb2fd
Kasia Nagel <katarzyna.nagel@wp.pl>
2017-11-18 09:48:15 +0100

    Dodawanie Modeli

---
+++ Fuel/Fuel/ViewModel/FuelViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fuel.ViewModel
+{
+    public class FuelViewModel
+    {
+        public FuelViewModel FuelModel { get; set; }
+        public bool ShowResult { get; set; }
+    }
+}
